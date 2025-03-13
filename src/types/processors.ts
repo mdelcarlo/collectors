bc9654bb2c647ad608df29ee3d6b0e3e7f7dafdd
Fran Espeche <fran.espeche@scale.com>
2025-03-13 14:22:46 -0300

    add AlignmentResult type

---
+++ src/types/processors.ts
@@ -0,0 +1,7 @@
+// Result got from python processor
+export type AlignmentResult = {
+  target: string; // path to the offseted video
+  offset: number; // offset in milliseconds
+  confidence: number; // 0-10, 10 being the most confident score
+  elapsed_time_seconds: number;
+};
