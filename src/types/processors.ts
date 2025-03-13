683cd15d99478a947e5acd752adacca02b02a579
Fran Espeche <fran.espeche@scale.com>
2025-03-13 16:50:34 -0300

    add one comment

--- src/types/processors.ts
+++ src/types/processors.ts
@@ -3,5 +3,5 @@ export type AlignmentResult = {
   target: string; // path to the offseted video
   offset: number; // offset in milliseconds
   confidence: number; // 0-10, 10 being the most confident score
-  elapsed_time_seconds: number;
+  elapsed_time_seconds: number; // time the process took in seconds
 };
