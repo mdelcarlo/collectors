838bbc4584129521cb180434c386f7d61b4386a9
Matias Del Carlo <matias.del@scale.com>
2025-03-14 13:37:14 -0300

    fix: fix base py to support 3.9 version

--- python/generators/downsamplers/base.py
+++ python/generators/downsamplers/base.py
@@ -1,19 +1,16 @@
-
 from abc import ABC, abstractmethod
-from typing import NewType
+from typing import NewType, Optional
 from utils.ENUMS import DEFAULT_OUTPUT_FPS, DEFAULT_OUTPUT_WIDTH
 
 # Types #
 OutputPath = NewType('OutputPath', str)
 
-class BaseVideoDownsampler (ABC):
+class BaseVideoDownsampler(ABC):
     def __init__(self):
-        self.result_path = None
+        self.result_path: Optional[OutputPath] = None  # Type hint for result_path
 
     @abstractmethod
     def process(self, input_file, output_dir, output_filename,
                 output_width=DEFAULT_OUTPUT_WIDTH,
-                output_fps=DEFAULT_OUTPUT_FPS) -> OutputPath | None:
+                output_fps=DEFAULT_OUTPUT_FPS) -> Optional[OutputPath]:  # Using Optional instead of Union
         pass
-
-
